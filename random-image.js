41ceddd11c732b0507702291407288a61ba2ecde
João Melo <jacbmelo@gmail.com>
2016-12-15 16:08:39 +0000

    Update random-image.js

--- random-image.js
+++ random-image.js
@@ -1,22 +1,32 @@
+/**
+ * UMD module exposing a method to return a random image URL.
+ * (c) 2016 jacbmelo
+ */
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
-      // AMD
+    
+    // AMD
     define(['jquery'], factory);
   } else if (typeof exports === 'object') {
-      // CommonJS
+    
+    // CommonJS
     module.exports = factory(require('jquery'));
   } else {
+    
     // Browser globals (Note: root is window)
     root.RandomImage = factory(root.$);
   }
-}(this, function ($) {
+} (this, function ($) {
   
-  // Methods
+  // Define Module
   var RandomImage = {
-    getUrl:function(callback) {
-      var jqxhr = $.getJSON('data.json').done(function(data) {
+    
+    /**
+     * Gets a random image URL from data.json and calls a callback function with this URL.
+     */
+    getUrl: function(callback) {
+      $.getJSON('data.json').done(function(data) {
         var url = data[Math.floor(Math.random() * data.length)];
-        console.log('url: ' + url);
         callback(url);
       }).fail(function(jqXHR, textStatus, errorThrown) {
         console.log(jqXHR);
@@ -26,6 +36,6 @@
     }
   }
 
-  // Exposed object
+  // Expose object
   return RandomImage;
 }));
